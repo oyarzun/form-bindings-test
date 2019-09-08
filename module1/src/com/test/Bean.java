3729ca8ebb9e9d05d3b2a7f421c342c2dcbe16d9
Christian Oyarzun <co@oyarzun.net>
2019-09-08 14:28:26 -0400

    initial commit

---
+++ module1/src/com/test/Bean.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test;
+
+/**
+ *
+ * @author oyarzun
+ */
+public class Bean {
+    private int intBool = -1;
+
+    public Bean() {
+    }
+
+    
+    
+    public int getIntBool() {
+        return intBool;
+    }
+
+    public void setIntBool(int intBool) {
+        this.intBool = intBool;
+    }
+    
+    
+}
